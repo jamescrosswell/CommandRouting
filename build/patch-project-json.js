8200323be4eb6d1e4eb4e3aa78167c06bb2e2f00
James Crosswell <james.crosswell@gmail.com>
2016-03-30 22:39:00 +0200

    simplified script to patche the version number in project.json

--- build/patch-project-json.js
+++ build/patch-project-json.js
@@ -6,27 +6,14 @@ if (!file)
     console.log("No filename provided");
 console.log("File: " + file);
 
-// Read in the build number (this might be provided by the CI server)
-var buildNumber = process.argv[3]; // e.g. '42'
-if (!buildNumber)
-    console.log("No build number provided");
-
-// Read in the optional release
-var release = process.argv[4]; // e.g. 'alpha'
-if (release)
-    buildNumber = buildNumber + "-" + release;
-console.log("Build: " + buildNumber);
+// Read in the build version (this might be provided by the CI server)
+var version = process.argv[3]; // e.g. '1.0.42-beta'
+if (!version)
+    console.log("No version provided");
 
 jsonfile.readFile(file, function (err, project) {
-    // Patch the project.version to replace the last integer component with
-    // the build version that was supplied as an argument
-    var findPoint       = project.version.lastIndexOf(".");
-    var basePackageVer  = project.version.substring(0, findPoint + 1);
-    var productVersion      = basePackageVer + buildNumber;
-    console.log("Version: " + productVersion);
-
-
-    project.version = productVersion;
+    // Patch the project.version 
+    project.version = version;
     jsonfile.writeFile(file, project, {spaces: 2}, function(err) {
         console.error(err);
     });
